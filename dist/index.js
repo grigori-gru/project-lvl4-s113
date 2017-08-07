bfb196fdb41cc04345a1be2e87fd20bde82f9347
Grigoriy Grudinin <grgr14@mail.ru>
2017-08-08 01:55:34 +0300

    second deploy

--- dist/index.js
+++ dist/index.js
@@ -8,18 +8,36 @@ var _asyncToGenerator2 = require('babel-runtime/helpers/asyncToGenerator');
 
 var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
 
+var _koa = require('koa');
+
+var _koa2 = _interopRequireDefault(_koa);
+
+var _koaRouter = require('koa-router');
+
+var _koaRouter2 = _interopRequireDefault(_koaRouter);
+
+var _rollbar = require('rollbar');
+
+var _rollbar2 = _interopRequireDefault(_rollbar);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var Koa = require('koa');
-var app = new Koa();
+var app = new _koa2.default();
+var router = new _koaRouter2.default();
 
-app.use(function () {
+// include and initialize the rollbar library with your access token
+var rollbar = new _rollbar2.default("8af6d3888b364772abefb2b4241d98f3");
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
+
+router.get('/', function () {
   var _ref = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee(ctx) {
     return _regenerator2.default.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            ctx.body = 'Hello World';
+            ctx.body = 'Router World';
 
           case 1:
           case 'end':
@@ -33,5 +51,10 @@ app.use(function () {
     return _ref.apply(this, arguments);
   };
 }());
+
+app.use(router.routes()).use(router.allowedMethods());
+
+var port = process.env.PORT || 3000;
+app.use(rollbar.errorHandler("8af6d3888b364772abefb2b4241d98f3"));
 
 app.listen(3000);
