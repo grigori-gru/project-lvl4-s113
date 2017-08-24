42fc83a4a1a790a92dc9aa04766c5c5fed3cc01a
Grigoriy Grudinin <grgr14@mail.ru>
2017-08-25 01:30:02 +0300

    add pg

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const dbURL = process.env.NODE_ENV === 'production'
-  ? process.env.DATABASE_URL_PROD
+  ? process.env.DATABASE_URL
   : process.env.DATABASE_URL_DEV;
 
 export default new Sequelize(dbURL);
